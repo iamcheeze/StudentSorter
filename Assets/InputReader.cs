a455b02a55416802b451a701fe0452cba063a8f8
iamcheeze <theglasstable2007@gmail.com>
2022-06-16 13:27:38 -0700

    First Check-in
    I forgot to make this a git project before hand so the project currently is basically just a small system that adds your students' names to a list that you input in the enter student name here field.

---
+++ Assets/InputReader.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InputReader : MonoBehaviour
+{
+    private string input;
+    public static List<string> studentList = new List<string>();
+    public TextMeshProUGUI textList;
+    public TMP_InputField inputFieldText;
+    public string s;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        s = inputFieldText.text;
+        textList.text = ListToText(studentList);
+    }
+
+    public void ReadStringInput(string input)
+    {
+        input = s;
+        studentList.Add(input);
+    }
+
+    private string ListToText(List<string> list)
+    {
+        string result = "";
+        foreach (var listMember in list)
+        {
+            result += listMember.ToString() + "\n";
+        }
+        return result;
+    }
+}
